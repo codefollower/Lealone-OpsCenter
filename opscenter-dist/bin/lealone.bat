7e2fbbbc8452d335b2305b6ec49764e0605b666c
Honghua Zhu <zhh200910@gmail.com>
2022-04-15 11:00:37 +0800

    启动脚本增加-debug参数

--- opscenter-dist/bin/lealone.bat
+++ opscenter-dist/bin/lealone.bat
@@ -1,18 +1,7 @@
 @REM
-@REM  Licensed to the Apache Software Foundation (ASF) under one or more
-@REM  contributor license agreements.  See the NOTICE file distributed with
-@REM  this work for additional information regarding copyright ownership.
-@REM  The ASF licenses this file to You under the Apache License, Version 2.0
-@REM  (the "License"); you may not use this file except in compliance with
-@REM  the License.  You may obtain a copy of the License at
-@REM
-@REM      http://www.apache.org/licenses/LICENSE-2.0
-@REM
-@REM  Unless required by applicable law or agreed to in writing, software
-@REM  distributed under the License is distributed on an "AS IS" BASIS,
-@REM  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-@REM  See the License for the specific language governing permissions and
-@REM  limitations under the License.
+@REM  Copyright Lealone Database Group.
+@REM  Licensed under the Server Side Public License, v 1.
+@REM  Initial Developer: zhh
 
 @echo off
 if "%OS%" == "Windows_NT" setlocal
@@ -42,12 +31,19 @@ REM  -XX:+UseCMSInitiatingOccupancyOnly
 set logdir=%LEALONE_HOME%\logs
 set args=""
 
+set JAVA_OPTS=-Xms10M^
+ -Dlealone.logdir="%logdir%"
+
 set str=%1
 if "%str%"=="-nodes" (
     goto nodes
 )
 if "%str%"=="-cluster" (
     goto cluster
+)
+if "%str%"=="-debug" (
+    set JAVA_OPTS=%JAVA_OPTS% -agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=y
+    goto exec
 )
 if "%str%"=="" (
     goto exec
@@ -68,21 +64,12 @@ set logdir="%logdir%\cluster\node%2"
 set args="-cluster %2"
 goto exec
 
-
 :exec
-set JAVA_OPTS=-Xms10M^
- -Dlealone.logdir="%logdir%"^
- -Dlealone.config.loader=org.lealone.aose.config.YamlConfigurationLoader
-
-REM set JAVA_OPTS=%JAVA_OPTS% -agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=y
-
-REM Ensure that any user defined CLASSPATH variables are not used on startup
 set CLASSPATH="%LEALONE_HOME%\conf;%LEALONE_HOME%\lib\*"
 
 REM echo Starting Lealone Server
-"%JAVA_HOME%\bin\java" %JAVA_OPTS% -cp %CLASSPATH% "%LEALONE_MAIN%" "%args%"
+"%JAVA_HOME%\bin\java" %JAVA_OPTS% -cp %CLASSPATH% %LEALONE_MAIN% %args%
 goto finally
-
 
 :err
 echo JAVA_HOME environment variable must be set!
--- opscenter-dist/bin/sqlshell.bat
+++ opscenter-dist/bin/sqlshell.bat
@@ -1,18 +1,7 @@
 @REM
-@REM  Licensed to the Apache Software Foundation (ASF) under one or more
-@REM  contributor license agreements.  See the NOTICE file distributed with
-@REM  this work for additional information regarding copyright ownership.
-@REM  The ASF licenses this file to You under the Apache License, Version 2.0
-@REM  (the "License"); you may not use this file except in compliance with
-@REM  the License.  You may obtain a copy of the License at
-@REM
-@REM      http://www.apache.org/licenses/LICENSE-2.0
-@REM
-@REM  Unless required by applicable law or agreed to in writing, software
-@REM  distributed under the License is distributed on an "AS IS" BASIS,
-@REM  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-@REM  See the License for the specific language governing permissions and
-@REM  limitations under the License.
+@REM  Copyright Lealone Database Group.
+@REM  Licensed under the Server Side Public License, v 1.
+@REM  Initial Developer: zhh
 
 @echo off
 if "%OS%" == "Windows_NT" setlocal
@@ -28,9 +17,6 @@ if NOT DEFINED LEALONE_MAIN set LEALONE_MAIN=org.lealone.main.Shell
 set JAVA_OPTS=-Xms10M^
  -Dlealone.logdir="%LEALONE_HOME%\logs"
 
-REM ***** CLASSPATH library setting *****
-
-REM Ensure that any user defined CLASSPATH variables are not used on startup
 set CLASSPATH="%LEALONE_HOME%\conf;%LEALONE_HOME%\lib\*"
 
 REM set LEALONE_PARAMS=%1 %2
