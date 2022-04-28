e53d45e307c007f51a9a698834eb271909b14f43
Honghua Zhu <zhh200910@gmail.com>
2022-04-29 07:18:28 +0800

    更新所有启动脚本

--- opscenter-dist/bin/runSqlScript.sh
+++ opscenter-dist/bin/runSqlScript.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+
+# Copyright Lealone Database Group.
+# Licensed under the Server Side Public License, v 1.
+# Initial Developer: zhh
+
 java -cp ../lib/lealone-opscenter-1.0.0.jar org.lealone.opscenter.main.OpsCenterSqlScript -serviceDir ../sql
 
